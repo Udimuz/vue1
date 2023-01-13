1ba3d83d075c5f31c21f9ad37c927b721da3fae0
Dima Laz <Udimuz@gmail.com>
2023-01-13 09:13:56 +0300

    2: Установлены расширения

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-center bg-success">
+    <div>111111111-444444444444</div>
+    <div>Имя: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostComponent",
+
+  data() {
+    return {
+      name: 'Дима',
+      age: 47,
+      arr: [11, '22', true],
+      someObj: {
+        personName: 'Damayita'
+      }
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
